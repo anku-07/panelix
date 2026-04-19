194715d6f49e84d767a00ecac2a154617a27f3cb
techAdi <adityamondal48765@gmail.com>
2026-04-19 22:05:22 +0530

    feat: refactor ProductsUI to use custom hook for product fetching and add search input component

--- components/ProductsUI.tsx
+++ components/ProductsUI.tsx
@@ -1,38 +1,39 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard/ProductCard";
-import productsApi from "../api/products.api";
+// import productsApi from "../api/products.api";
+// import { IProduct } from "@/typescript/interfaces/CustomAllInterface";
+import CommonInput from "@/ui/CommonInput/CommonInput";
+
+import useProduct from "@/hooks/useProduct";
 import { IProduct } from "@/typescript/interfaces/CustomAllInterface";
 
 const ProductsUI = () => {
-  const [products, setProducts] = useState<IProduct[]>([]);
+  const { products, loading, error } = useProduct();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await productsApi.getAllProducts();
-
-      setProducts(data?.products || []);
-    };
-
-    fetchData();
-  }, []);
-
-  console.log("all Product fetch", products);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
-    <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {products.map((item: IProduct) => (
-        <ProductCard
-          key={item.id}
-          id={item.id}
-          imageUrl={item.images?.[0] || "/fallback.png"}
-          category={item.category}
-          title={item.title}
-          description={item.description}
-          price={item.price}
-        />
-      ))}
+    <div>
+      <div className="mb-4">
+        <div>
+          <CommonInput placeholder="Search Product" />
+        </div>
+      </div>
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {products.map((item: IProduct) => (
+          <ProductCard
+            key={item.id}
+            id={item.id}
+            imageUrl={item.images?.[0] || "/fallback.png"}
+            category={item.category}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- hooks/useProduct.js
+++ hooks/useProduct.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import productsApi from "../api/products.api";
 
 const useProduct = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-// const 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await productsApi.getAllProducts();
+        setProducts(data?.products || []);
+      } catch (error) {
+        setError("Failed to fetch products", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchData();
+  }, []);
+
+  return { products, loading, error };
 };
 
 export default useProduct;
